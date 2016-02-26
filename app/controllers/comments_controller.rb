caccdb49502d422b1e4e43f1184451ef817f5a38
Freika <iamfrey@gmail.com>
2016-02-26 20:21:16 +0600

    Show course url on course page.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,7 +10,7 @@ class CommentsController < ApplicationController
     respond_to do |format|
       if @comment.save
         format.html do
-          redirect_to @course, notice: 'Comment was successfully created.'
+          redirect_to @course, notice: 'Спасибо за ваш комментарий!.'
         end
         format.json { render :show, status: :created, location: @comment }
       else
@@ -23,7 +23,7 @@ class CommentsController < ApplicationController
   def update
     respond_to do |format|
       if @comment.update(comment_params)
-        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
+        format.html { redirect_to @comment, notice: 'Комментарий успешно обновлен.' }
         format.json { render :show, status: :ok, location: @comment }
       else
         format.html { render :edit }
@@ -35,7 +35,7 @@ class CommentsController < ApplicationController
   def destroy
     @comment.destroy
     respond_to do |format|
-      format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
+      format.html { redirect_to comments_url, notice: 'Комментарий успешно удален.' }
       format.json { head :no_content }
     end
   end
--- app/models/course.rb
+++ app/models/course.rb
@@ -3,6 +3,8 @@ class Course < ActiveRecord::Base
   has_many :graduates
   has_many :users, through: :graduates
 
+  validates :url, :title, :description, :language, :paid, presence: true
+
   def rating
     scores = self.comments.pluck(:rating)
     scores.inject{ |sum, el| sum + el } / scores.size if scores.any?
