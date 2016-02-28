5f5b274f710caf72d37328df221535843c953cb9
Freika <iamfrey@gmail.com>
2016-02-28 19:26:23 +0600

    Fix couple bugs and disable comment submit button if field is empty.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,4 +25,20 @@ $(document).on('ready page:change', function() {
     else
       $('.rate-course').fadeOut('fast');
   });
+
+  $('#comment_content').keyup(function() {
+
+    var empty = false;
+    $('#comment_content').each(function() {
+      if ($(this).val().length == 0) {
+        empty = true;
+      }
+    });
+
+    if (empty) {
+      $('#submit_comment').attr('disabled', 'disabled');
+    } else {
+      $('#submit_comment').removeAttr('disabled');
+    }
+  });
 });
