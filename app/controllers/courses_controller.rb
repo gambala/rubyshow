f385da057c7840edc686eb29a2c4c825b43232b7
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2016-03-29 02:36:25 +0300

    Make courses filtration form with form_tag helper

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,11 +1,12 @@
 class CoursesController < ApplicationController
   load_and_authorize_resource
   before_action :set_course, only: [:show, :edit, :update, :destroy, :approve]
-  helper_method :courses_title, :courses_search_title, :courses_overview_title
+  helper_method :courses_title, :courses_search_title, :courses_overview_title,
+                :language_options, :paid_options
 
   def index
     courses = CoursesQuery.new(courses_filter_params, Course.includes(:comments))
-    render locals: { courses: courses, courses_title: courses_title }
+    render locals: { courses: courses }
   end
 
   def show
@@ -103,6 +104,16 @@ class CoursesController < ApplicationController
     else
       ''
     end
+  end
+
+  def language_options
+    language_options ||= [['На всех языках', 'all'], 'Русский', 'English']
+  end
+
+  def paid_options
+    paid_options ||= [['И платные, и бесплатные', 'all'],
+                      ['Только платные', '1'],
+                      ['Только бесплатные', '0']]
   end
 
   def set_course
---
+++ app/services/courses_filter_titling_service.rb
@@ -0,0 +1,11 @@
+class CoursesFilterTitlingService
+  attr_accessor :options
+
+  def initialize(options)
+    @options = options
+  end
+
+  def titles
+    {}
+  end
+end
