eefab1ddea85e31762271e5601fd2638f3f5a0c9
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2016-03-29 03:36:16 +0300

    Move query titling logic into separate service and stylize output

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,12 +1,12 @@
 class CoursesController < ApplicationController
   load_and_authorize_resource
   before_action :set_course, only: [:show, :edit, :update, :destroy, :approve]
-  helper_method :courses_title, :courses_search_title, :courses_overview_title,
-                :language_options, :paid_options
+  helper_method :language_options, :paid_options
 
   def index
     courses = CoursesQuery.new(courses_filter_params, Course.includes(:comments))
-    render locals: { courses: courses }
+    page_titles = CoursesFilterTitlingService.new(courses_filter_params, courses)
+    render locals: { courses: courses, page_titles: page_titles }
   end
 
   def show
@@ -51,60 +51,6 @@ class CoursesController < ApplicationController
   end
 
   private
-
-  def courses_title
-    @courses_title ||= "#{paid_param_title} по Ruby #{language_param_title}"
-  end
-
-  def courses_search_title
-    @courses_search_title ||= if params[:title].present?
-                                "Созвучные с «#{params[:title]}»"
-                              else
-                                ''
-                              end
-  end
-
-  def courses_overview_title
-    @courses_overview_title ||=
-      "#{courses_amount_title} #{comments_amount_title} #{courses_suffix}"
-  end
-
-  def courses_amount_title
-    courses_size = 1
-    "#{courses_size} #{t('words.course', count: courses_size)}"
-  end
-
-  def comments_amount_title
-    comments_size = 0
-    "и #{comments_size} #{t('words.comment', count: comments_size)}"
-  end
-
-  def courses_suffix
-    courses_size = 1
-    t('words.courses_suffix', count: courses_size)
-  end
-
-  def paid_param_title
-    case params[:paid]
-    when '1'
-      'Платные курсы'
-    when '0'
-      'Бесплатные курсы'
-    else
-      'Курсы'
-    end
-  end
-
-  def language_param_title
-    case params[:language]
-    when 'Русский'
-      'на русском'
-    when 'English'
-      'на английском'
-    else
-      ''
-    end
-  end
 
   def language_options
     language_options ||= [['На всех языках', 'all'], 'Русский', 'English']
