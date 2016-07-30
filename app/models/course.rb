e335b08e9751d6d29da877167635d90ed673306b
Badichan Dmitriy <badimalex@gmail.com>
2016-07-30 16:49:45 +0300

    New rating

--- app/models/course.rb
+++ app/models/course.rb
@@ -20,7 +20,16 @@ class Course < ActiveRecord::Base
 
   def rating
     return 0 unless scores.any?
-    (scores.inject { |sum, element| sum + element }.to_f / scores.size).try(:round, 1)
+    ActiveRecord::Base.connection.exec_query("SELECT round(((LOG(
+                     (SELECT power((SELECT MAX(count_votes)
+                                      FROM
+                                        (SELECT count(comments.id) AS count_votes
+                                         FROM comments
+                                         LEFT JOIN courses ON comments.course_id=courses.id
+                                         GROUP BY courses.id) t), 0.2)), count(com.id)))+rating)/2, 2) AS actual_raiting
+          FROM courses c
+          LEFT JOIN comments com ON c.id=com.course_id
+          WHERE c.id=#{id} GROUP BY c.id, com.rating").first['actual_raiting']
   end
 
   private
