51624a1c93e012a1e20df3db0bf7590ee8345da7
Freika <iamfrey@gmail.com>
2016-11-01 19:24:04 +0700

    Add free and paid routes and handle cancan exception with redirect.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, notice: exception.message
+  end
+
   protected
 
   def configure_permitted_parameters
