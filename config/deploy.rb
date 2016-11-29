cb47b57118a966459d853abc012fe7b054e5a2e5
Freika <iamfrey@gmail.com>
2016-11-29 20:16:34 +0700

    Update deploy.rb.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,29 +1,20 @@
-set :application, 'ruby_courses'
-set :deploy_user, 'deploy'
+set :application, 'rubycourses'
+set :repo_url, 'git@github.com:Freika/nick.git'
 
-# setup repo details
-set :scm, :git
-set :repo_url, 'git@github.com:Freika/ruby_courses.git'
+set :deploy_to, '/home/deploy/var/www/rubycourses'
 
-# setup rvm.
-set :rbenv_type, :system
-set :rbenv_ruby, '2.3.0'
-set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-set :rbenv_map_bins, %w{rake gem bundle ruby rails}
-
-# how many old releases do we want to keep, not much
-set :keep_releases, 5
-
-# files we want symlinking to specific entries in shared
-set :linked_files, %w{config/database.yml config/secrets.yml config/application.yml}
-
-# dirs we want symlinking to shared
+set :linked_files, %w{config/database.yml}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
-# this:
-# http://www.capistranorb.com/documentation/getting-started/flow/
-# is worth reading for a quick overview of what tasks are called
-# and when for `cap stage deploy`
-
 namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :publishing, 'deploy:restart'
+  after :finishing, 'deploy:cleanup'
 end
