9112849aa59f59ee9694d7d2701a44c824d09fce
Eugene Burmakin <iamfrey@gmail.com>
2017-05-11 19:07:23 +0700

    Add libsodium.

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
+require 'rbnacl/libsodium'
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
