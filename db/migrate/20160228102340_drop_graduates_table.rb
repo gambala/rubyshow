29722d222ea2c60cd08415325706419daac65567
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2017-08-02 14:05:31 +0300

    Make all migrations reversible

--- db/migrate/20160228102340_drop_graduates_table.rb
+++ db/migrate/20160228102340_drop_graduates_table.rb
@@ -1,7 +1,17 @@
 # frozen_string_literal: true
 
 class DropGraduatesTable < ActiveRecord::Migration
-  def change
+  def up
     drop_table :graduates
+  end
+
+  def down
+    create_table :graduates do |t|
+      t.boolean :finished
+      t.integer :course_id
+      t.integer :user_id
+
+      t.timestamps null: false
+    end
   end
 end
