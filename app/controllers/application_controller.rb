420e967e371b916575db15d104a4bb9045e88370
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2017-08-02 13:52:53 +0300

    Use more semantically correct referer method instead of referrer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,6 +20,6 @@ class ApplicationController < ActionController::Base
 
   def user_not_authorized
     flash[:alert] = 'У вас недостаточно прав для выполнения этого действия.'
-    redirect_to(request.referrer || root_path)
+    redirect_to(request.referer || root_path)
   end
 end
