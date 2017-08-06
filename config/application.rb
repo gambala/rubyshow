2e80bf8f2c73ff8e003ff87502913f0075347dda
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2017-08-06 23:44:37 +0300

    Clean up initializers

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,6 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails'
-# Pick the frameworks you want:
 require 'active_model/railtie'
 require 'active_job/railtie'
 require 'active_record/railtie'
@@ -12,41 +11,23 @@ require 'action_mailer/railtie'
 require 'action_view/railtie'
 require 'sprockets/railtie'
 require 'rbnacl/libsodium'
-# require "rails/test_unit/railtie"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module RubyCourses
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
-
-    # Do not swallow errors in after_commit/after_rollback callbacks.
     config.action_view.field_error_proc = proc { |html_tag, _instance| html_tag }
     config.active_record.raise_in_transactional_callbacks = true
     config.autoload_paths += %W(
       #{config.root}/app/presenters
       #{config.root}/app/queries
     )
-    config.generators do |g|
-      g.test_framework :rspec,
-                       view_specs:    false,
-                       request_specs: false,
-                       routing_specs: false
-    end
     config.encoding = 'utf-8'
     config.i18n.available_locales = %i(ru en)
     config.i18n.default_locale = :ru
+
+    config.generators do |g|
+      g.test_framework :rspec, view_specs: false, request_specs: false, routing_specs: false
+    end
   end
 end
--- config/boot.rb
+++ config/boot.rb
@@ -2,7 +2,7 @@
 
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
-require 'bundler/setup' # Set up gems listed in the Gemfile.
+require 'bundler/setup'
 require 'bootsnap'
 
 Bootsnap.setup(
