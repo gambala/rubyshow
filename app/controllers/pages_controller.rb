c655d53e125848de068f968b98a7eed0b9efb922
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2017-08-09 17:10:58 +0300

    Name env variables with courses and comments more semantically correctly

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,11 +2,11 @@
 
 class PagesController < ApplicationController
   def home
-    @courses_number = Course.count
-    @comments_number = Comment.count
+    @courses_size = Course.count
+    @comments_size = Comment.count
     @random_course = Course.all.sample
     @random_comment = Comment.includes(:user, :course).all.sample
-    @top_5_free = CoursesQuery.new(Course.free).filter_top_5.courses
-    @top_5_paid = CoursesQuery.new(Course.paid).filter_top_5.courses
+    @top_5_free_courses = CoursesQuery.new(Course.free).filter_top_5.courses
+    @top_5_paid_courses = CoursesQuery.new(Course.paid).filter_top_5.courses
   end
 end
