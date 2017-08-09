6d4ef8ea907b92596651286c4594f34e04a81c51
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2017-08-10 00:53:42 +0300

    Update simplecov configs in test suite by recommendation from them
    https://github.com/codeclimate/ruby-test-reporter/blob/master/README.md

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-require 'codeclimate-test-reporter'
-CodeClimate::TestReporter.start
+require 'simplecov'
+SimpleCov.start
 
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
