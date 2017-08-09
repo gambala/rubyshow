3c6d37b0ca627b50539d271cda6ae5cb25c90481
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2017-08-10 00:43:09 +0300

    Use lazy method helpers instead of env variables on welcome index page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,35 @@
 # frozen_string_literal: true
 
 class WelcomeController < ApplicationController
-  def index
-    @courses_size = Course.count
-    @comments_size = Comment.count
-    @random_course = Course.all.sample
-    @random_comment = Comment.includes(:user, :course).all.sample
-    @top_5_free_courses = CoursesQuery.new(Course.free).filter_top_5.courses
-    @top_5_paid_courses = CoursesQuery.new(Course.paid).filter_top_5.courses
+  helper_method :comments_size, :courses_size,
+                :random_comment, :random_course,
+                :top_5_free_courses, :top_5_paid_courses
+
+  def index; end
+
+  private
+
+  def comments_size
+    @comments_size ||= Comment.count
+  end
+
+  def courses_size
+    @courses_size ||= Course.count
+  end
+
+  def random_comment
+    @random_comment ||= Comment.includes(:user, :course).all.sample
+  end
+
+  def random_course
+    @random_course ||= Course.all.sample
+  end
+
+  def top_5_free_courses
+    @top_5_free_courses ||= CoursesQuery.new(Course.free).filter_top_5.courses
+  end
+
+  def top_5_paid_courses
+    @top_5_paid_courses ||= CoursesQuery.new(Course.paid).filter_top_5.courses
   end
 end
