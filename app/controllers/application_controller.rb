5cf233956371f1eabf75501c7602768fdc463e95
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2017-08-10 00:36:15 +0300

    Remove almost unnecessary protected section from classes and modules

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,15 +8,13 @@ class ApplicationController < ActionController::Base
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
-  protected
+  private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i(username email password password_confirmation remember_me))
     devise_parameter_sanitizer.permit(:sign_in, keys: %i(login username email password remember_me))
     devise_parameter_sanitizer.permit(:account_update, keys: %i(username email password password_confirmation current_password))
   end
-
-  private
 
   def user_not_authorized
     flash[:alert] = 'У вас недостаточно прав для выполнения этого действия.'
