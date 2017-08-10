81f7655b1b6bffe94f29cdcfb3f44a5a03d28cee
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2017-08-10 10:28:49 +0300

    Add node_modules folder to assets path

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
-additional_paths = ["#{Gem.loaded_specs['breakpoint'].full_gem_path}/stylesheets"]
+additional_paths = [Rails.root.join('node_modules'),
+                    "#{Gem.loaded_specs['breakpoint'].full_gem_path}/stylesheets"]
 
 Rails.application.config.assets.paths += additional_paths
 Rails.application.config.assets.precompile += %w(modernizr-custom.js)
