669ba239e5d0f9fa8e8e540e4817520c32e5d616
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2017-08-20 18:23:10 +0300

    Use new modernizr bundle in es6 pack and remove old one from vendor dir

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,1 @@
-console.log('Hello World from Webpacker')
+import 'bundles/modernizr';
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,5 +4,6 @@ additional_paths = [Rails.root.join('node_modules'),
                     "#{Gem.loaded_specs['breakpoint'].full_gem_path}/stylesheets"]
 
 Rails.application.config.assets.paths += additional_paths
-Rails.application.config.assets.precompile += %w(modernizr-custom.js)
 Rails.application.config.assets.version = '1.0'
+
+# Rails.application.config.assets.precompile += %w(modernizr-custom.js)
