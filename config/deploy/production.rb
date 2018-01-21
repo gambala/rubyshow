e7f9d1d3918f135e895e02fd9db80ceaa74fdabb
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2018-01-21 11:52:23 +0300

    Change domain to rubycourses.ru

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@ require_relative 'forbidden_locations'
 
 server ENV['deploy_host'], port: ENV['deploy_port'], user: ENV['deploy_user'], roles: %w(app db web)
 set :forbidden_locations, Deploy.forbidden_locations
-set :letsencrypt_path, 'rubycourses.gambala.pro'
+set :letsencrypt_path, 'rubycourses.ru'
 set :nginx_config_name, 'ruby_courses.production.nginx.conf'
-set :nginx_server_name, 'rubycourses.gambala.pro'
+set :nginx_server_name, 'rubycourses.ru'
 set :nginx_use_ssl, false
 set :ssh_options, keys: %w(/home/gambala/.ssh/id_rsa),
                   forward_agent: true,
