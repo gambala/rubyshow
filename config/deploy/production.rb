4cbdbf0945b78773599cf6136c16155fae8df507
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2020-04-29 19:26:54 +0300

    Do not use local ssh key for deploy anymore

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,6 +8,3 @@ set :letsencrypt_path, 'rubycourses.ru'
 set :nginx_config_name, 'ruby_courses.production.nginx.conf'
 set :nginx_server_name, 'rubycourses.ru'
 set :nginx_use_ssl, true
-set :ssh_options, keys: %w(/home/gambala/.ssh/id_rsa),
-                  forward_agent: true,
-                  auth_methods: %w(publickey)
