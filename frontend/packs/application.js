0161804753df2b6705444cde4d03d4352cf5b21f
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2020-05-07 04:21:00 +0300

    Restylize alerts with custom classes and scripts

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -5,7 +5,10 @@ import 'stylesheets';
 import Rails      from 'rails-ujs';
 import Turbolinks from 'turbolinks';
 
+import FlashItems from 'modules/FlashItems';
+
 window.Rails = Rails;
 
+FlashItems.start();
 Rails.start();
 Turbolinks.start();
