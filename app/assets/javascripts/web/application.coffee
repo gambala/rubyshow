d3a4d6e8f90d6e6f86d56bf6aae4c8c52aa5c3dd
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2020-05-07 04:22:15 +0300

    Remove unused now alert BS module

--- app/assets/javascripts/web/application.coffee
+++ app/assets/javascripts/web/application.coffee
@@ -1,6 +1,5 @@
 #= require jquery/dist/jquery
 #= require bootstrap/transition
-#= require bootstrap/alert
 #= require bootstrap/collapse
 #= require bootstrap/dropdown
 #= require bootstrap/tab
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,15 +3,6 @@
 module ApplicationHelper
   include Pagy::Frontend
 
-  def alert_class_for(flash_type)
-    {
-      success: 'alert-success',
-      error: 'alert-danger',
-      alert: 'alert-warning',
-      notice: 'alert-info'
-    }[flash_type.to_sym] || flash_type.to_s
-  end
-
   def avatar_url(email, size)
     gravatar_id = Digest::MD5.hexdigest(email.downcase)
     "https://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
