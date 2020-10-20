33382a3d110571ed3adfbe452038a09c901b0f85
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2020-10-20 02:09:57 +0200

    Split js pack into chunks

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,8 @@
-const { environment } = require('@rails/webpacker')
+const WebpackAssetsManifest = require('webpack-assets-manifest');
+const { environment } = require('@rails/webpacker');
 const erb = require('./loaders/erb');
 
 environment.loaders.prepend('erb', erb);
+environment.splitChunks();
 
-module.exports = environment
+module.exports = environment;
