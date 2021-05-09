14f51af757a85048b8630ce93f5dbfd54d45c395
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2021-05-09 18:37:22 +0200

    Move 4 helper method into rails-plus gem

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,23 +1,15 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  include Heroicon::Engine.helpers
   include Pagy::Frontend
+  include RailsPlus::Helpers::Benchmark
   include RailsPlus::Helpers::Dashed
+  include RailsPlus::Helpers::GravatarURL
+  include RailsPlus::Helpers::Hicon
+  include RailsPlus::Helpers::RescuedCsrfMetaTags
   include RailsPlus::Helpers::RichFormFor
   # include RailsPlus::Helpers::Seo
-
-  def avatar_url(email, size)
-    gravatar_id = Digest::MD5.hexdigest(email.downcase)
-    "https://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
-  end
-
-  def benchmark(caption = nil, &block)
-    Benchmark.benchmark(caption.to_s) do |x|
-      x.report do
-        yield
-      end
-    end
-  end
 
   def comments_amount
     Comment.count
@@ -36,21 +28,10 @@ module ApplicationHelper
     course.paid? ? 'Платный' : 'Бесплатный'
   end
 
-  def course_comment_count(course)
-    course&.comments&.count || 0
-  end
-
   def present(object, klass = nil)
     klass ||= "#{object.class}Presenter".constantize
     presenter = klass.new(object, self)
     yield presenter if block_given?
     presenter
-  end
-
-  def rescued_csrf_meta_tags
-    csrf_meta_tags
-  rescue ArgumentError
-    request.reset_session
-    csrf_meta_tags
   end
 end
