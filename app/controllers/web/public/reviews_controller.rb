cbb722d466d30ccd64054f6c5d77c45c9c88543e
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2021-05-16 08:42:33 +0200

    Test onefile multiple classes with zeitwerk

--- app/controllers/web/public/reviews_controller.rb
+++ app/controllers/web/public/reviews_controller.rb
@@ -1,11 +1,19 @@
 # frozen_string_literal: true
 
+class Web::Public::Review < Comment
+end
+
+class Web::Public::ReviewPolicy < ApplicationPolicy
+  def index?; true end
+end
+
 module Web::Public
   class ReviewsController < Web::Public::ApplicationController
     include WithPagy
 
     def index
-      @comments = Comment.includes(:course, :user).order(created_at: :desc)
+      @comments = Web::Public::Review.includes(:course, :user).order(created_at: :desc)
+      authorize @comments
       @comments = paginate(@comments, items: 6)
     end
   end
