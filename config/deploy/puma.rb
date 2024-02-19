37ee9841b36cebb182af9a88f535a022b1b72527
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2024-02-19 14:58:11 +0100

    Remove mina deploy files

--- config/deploy/puma.rb
+++
@@ -1,8 +0,0 @@
-# Puma
-namespace :puma do
-  desc 'Update puma config.'
-  task :config do
-    puma_config = erb("#{fetch(:template_path)}/puma.rb.erb")
-    command %(echo '#{puma_config}' > #{fetch(:shared_path)}/config/puma.rb)
-  end
-end
--- config/deploy/secrets.rb
+++
@@ -1,9 +0,0 @@
-# Secrets
-# TODO: Don't use exec
-namespace :secrets do
-  desc 'Upload secret configuration files.'
-  task :upload do
-    exec "scp -i #{fetch(:identity_file)} #{fetch(:app_root)}/config/{application,secrets}.yml " \
-         "#{file} #{fetch(:user)}@#{fetch(:domain)}:#{fetch(:shared_path)}/config/"
-  end
-end
