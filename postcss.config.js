b6e89d2f607efb202267f3f2db3a33805d4c524a
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2024-07-13 20:57:22 +0200

    Update tailwind; Clean up postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,7 @@
 module.exports = {
-  plugins: [
-    require("postcss-import"),
-    require("postcss-flexbugs-fixes"),
-    require("postcss-preset-env")({
-      autoprefixer: {
-        flexbox: "no-2009",
-      },
-      features: {
-        "nesting-rules": true,
-      },
-      stage: 3,
-    }),
-    require("autoprefixer"),
-    require("tailwindcss/nesting"),
-    require("tailwindcss")("./tailwind.config.js"),
-  ],
+  plugins: {
+    'postcss-import': {},
+    tailwindcss: {},
+    autoprefixer: {},
+  }
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,60 +1,10 @@
-const colors = require('tailwindcss/colors');
-
 module.exports = {
-  corePlugins: {
-    container: false,
-    preflight: false,
-  },
-  mode: 'jit',
-  purge: [
-    './app/components/**/*.rb',
-    './app/components/**/*.slim',
-    './app/helpers/**/*.rb',
+  content: [
     './app/views/**/*.erb',
     './app/views/**/*.html',
     './app/views/**/*.slim',
-    './frontend/**/*.js',
-  ],
-  variants: {
-    gradientColorStops: ['responsive', 'hover', 'focus', 'active'],
-  },
-  theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      black: colors.black,
-      white: colors.white,
-      gray: colors.coolGray,
-      red: colors.red,
-      orange: colors.orange,
-      yellow: colors.amber,
-      green: colors.green,
-      teal: colors.teal,
-      blue: colors.blue,
-      indigo: colors.indigo,
-      purple: colors.purple,
-    },
-    fontSize: {
-      'xs':   ['0.75rem', '1rem'],
-      'sm':   ['0.875rem', '1.25rem'],
-      'base': ['1rem', '1.5rem'],
-      'lg':   ['1.125rem', '1.75rem'],
-      'xl':   ['1.25rem', '1.75rem'],
-      '2xl':  ['1.5rem', '2rem'],
-      '3xl':  ['1.875rem', '2.25rem'],
-      '4xl':  ['2.25rem', '2.5rem'],
-      '5xl':  ['3rem', '1.17'],
-      '6xl':  ['3.75rem', '1.17'],
-      '7xl':  ['4.5rem', '1.17'],
-      '8xl':  ['6rem', '1.17'],
-      '9xl':  ['8rem', '1.17'],
-    },
-    screens: {
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1536px',
-    },
-  },
+    './app/helpers/**/*.rb',
+    './app/assets/stylesheets/**/*.css',
+    './app/javascript/**/*.js'
+  ]
 }
