2d312396b0773311f877caa6aeef771593a781fe
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2025-05-23 12:06:12 +0400

    Update TailwindCSS to 4

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  content: [
-    './app/views/**/*.erb',
-    './app/views/**/*.html',
-    './app/views/**/*.slim',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js'
-  ]
-}
